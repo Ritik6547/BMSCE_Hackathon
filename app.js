366cb7d9650117bfa13ab3ba22d938274911ab55
Ritik <alphaop6547@gmail.com>
2023-08-19 04:41:24 +0530

    connectDB() configuration

--- app.js
+++ app.js
@@ -11,6 +11,9 @@ const app = express()
 
 const PORT = process.env.PORT || 8080
 
+// to connect to database uncomment this connectDB() function and add connection string as MONGO_URI in .env file
+// connectDB(); 
+
 // set view engine
 app.set('view engine', 'ejs')
 app.set("views", path.resolve(__dirname, "views"))
@@ -21,18 +24,11 @@ app.use(express.static('public'))
 // routes
 app.use('/', goalRouter)
 
-const start = async () => {
-    try {
-        await connectDB()
-        app.listen(PORT, () => {
-            console.log(`Server is running on http://localhost:${PORT}`)
-        })
-    } catch (error) {
-        console.log(error);
-    }
-};
 
-start();
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`)
+})
+
 
 
 
